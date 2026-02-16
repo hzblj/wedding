8d26cd556f9e7198f124cbb3a2df4f036f32b77b
Jan Blazej <jan.blazej@footshop.cz>
2026-02-16 22:29:06 +0100

    Better background

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -8,9 +8,9 @@ type Props = Readonly<{
 
 export default function RootLayout({children}: Props) {
   return (
-    <div className="min-h-screen w-auto bg-black flex flex-col flex-wrap items-center gap-0 p-0 overflow-visible relative">
+    <div className="min-h-screen w-auto app-background grain flex flex-col flex-wrap items-center gap-0 p-0 overflow-visible relative">
       <Navigation />
-      <div className="min-h-screen h-min p-0 relative bg-black flex-col flex-wrap w-full contents">
+      <div className="min-h-screen h-min p-0 relative flex-col flex-wrap w-full contents">
         <main className="flex flex-col flex-wrap flex-none items-start gap-0 w-full max-w-450 h-min p-0 relative">
           {children}
         </main>
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,5 +1,5 @@
 export const Navigation = () => (
-  <nav className="fixed top-0 left-0 right-0 z-50 bg-black flex justify-center">
+  <nav className="fixed top-0 left-0 right-0 z-1001 bg-transparent backdrop-blur-lg flex justify-center">
     <div className="flex flex-row place-content-[center_space-between] w-full max-w-full h-min py-3 px-8 relative overflow-hidden">
       <span className="text-white font-normal text-[14px]">[ Karin & Jan ]</span>
     </div>
