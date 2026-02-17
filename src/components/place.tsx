5fb2ffbb85151772f142ded63b6793f04c2fe7db
Jan Blazej <jan.blazej@footshop.cz>
2026-02-17 15:00:49 +0100

    Section 24px for mobile

--- src/components/place.tsx
+++ src/components/place.tsx
@@ -23,7 +23,7 @@ const PlaceImagePaper = ({variant = 'bottom-right'}: PlaceImagePaperProps) => (
 )
 
 const PlaceImage = () => (
-  <div className="flex-1 w-full md:w-px max-w-110 aspect-[0.75] md:aspect-auto md:h-110 md:max-h-110 xl:h-150 xl:max-h-150 relative">
+  <div className="flex-1 w-full md:w-px max-w-110 aspect-[0.75] md:aspect-auto md:h-110 md:max-h-110 xl:h-150 xl:max-h-150 relative p-8">
     <div className="paper-texture bg-white h-full max-h-full max-w-full w-full -rotate-[2.5deg] p-[16px_16px_64px]">
       <div className="z-1 flex-none absolute inset-[16px_16px_64px] overflow-hidden">
         <Image src="https://picsum.photos/id/1/800/1200" alt="place" fill className="object-cover" />
