58baf2b66c66901e40bb5516661075a949858f49
Jan Blazej <jan.blazej@footshop.cz>
2026-02-17 13:54:21 +0100

    Fixed double divider

--- src/components/ui/section.tsx
+++ src/components/ui/section.tsx
@@ -6,7 +6,7 @@ export type SectionProps = {
 }
 
 export const Section: FC<SectionProps> = ({right, left}) => (
-  <section className="flex flex-col md:flex-row flex-none place-content-[center_flex-start] items-center gap-0 w-full h-auto md:h-screen md:max-h-200 p-0 relative overflow-hidden border-y border-solid border-white/10">
+  <section className="flex flex-col md:flex-row flex-none place-content-[center_flex-start] items-center gap-0 w-full h-auto md:h-screen md:max-h-200 p-0 relative overflow-hidden border-t border-solid border-white/10">
     <div className="flex flex-col flex-1 place-content-center items-center gap-0 w-full md:w-px min-h-150 md:min-h-0 md:h-full p-0 relative">
       <div className="flex flex-row flex-1 place-content-center items-center gap-0 w-full h-full md:h-px p-16 xl:p-8 relative">
         {left}
