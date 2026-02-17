10e0d7d0f0b97b534539e2b4dcbcce58b08744d1
Jan Blazej <jan.blazej@footshop.cz>
2026-02-17 14:53:51 +0100

    Section enhancing

--- src/components/ui/section.tsx
+++ src/components/ui/section.tsx
@@ -7,14 +7,14 @@ export type SectionProps = {
 
 export const Section: FC<SectionProps> = ({right, left}) => (
   <section className="flex flex-col md:flex-row flex-none place-content-[center_flex-start] items-center gap-0 w-full h-auto md:h-screen md:max-h-200 p-0 relative overflow-hidden border-t border-solid border-white/10">
-    <div className="flex flex-col flex-1 place-content-center items-center gap-0 w-full md:w-px min-h-150 md:min-h-0 md:h-full p-0 relative">
+    <div className="flex flex-col flex-1 place-content-center items-center gap-0 w-full md:w-px md:min-h-0 md:h-full p-0 relative">
       <div className="flex flex-row flex-1 place-content-center items-center gap-0 w-full h-full md:h-px p-16 xl:p-8 relative">
         {left}
       </div>
     </div>
     <div className="hidden md:block w-px h-full bg-white/10" />
     <div className="flex flex-col flex-1 place-content-center items-center gap-4 w-full md:w-px h-auto md:h-full p-0 relative">
-      <div className="flex flex-col flex-1 place-content-center items-start gap-6 w-full h-auto md:h-px p-8 relative">
+      <div className="flex flex-col flex-1 place-content-center items-start gap-6 w-full h-auto md:h-px pt-6 pb-18 px-8 md:p-8 relative">
         {right}
       </div>
     </div>
