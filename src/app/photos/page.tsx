9d8db50ee8bd16fe9aa14592e9b2dbf29eedfa09
Jan Blazej <jan.blazej@footshop.cz>
2026-03-14 18:33:54 +0100

    Photos texts

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -6,12 +6,11 @@ import {SectionParagraph, SectionTitle} from '@/ui'
 
 const PageTitle = () => (
   <section className="flex flex-col gap-6 items-center h-min overflow-hidden w-full p-0 flex-[0_0_auto]">
-    <SectionTitle eyebrow="Share" className="w-full items-center text-center">
-      Photos
+    <SectionTitle eyebrow="Sdílej" className="w-full items-center text-center">
+      Fotky
     </SectionTitle>
     <SectionParagraph className="max-w-130 text-center">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec suscipit auctor dui, sed efficitur ligula. Donec a
-      nunc eget nisl convallis commodo. Donec ut nisi sed enim efficitur efficitur.
+      Budeme rádi, když s námi budete sdílet fotografie z celého dne.
     </SectionParagraph>
     <PhotoUpload />
   </section>
